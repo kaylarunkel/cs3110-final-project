857fcae9e63dc11f3580b9eddf5b61eeceae031c
Ilia Raiszadeh <ipr6@cornell.edu>
2024-05-15 15:22:06 -0400

    Added full comments for all functions in .mli files Ilia Raiszadeh (ipr6)

--- expense_tracker/lib/textbox.mli
+++ expense_tracker/lib/textbox.mli
@@ -21,3 +21,8 @@ val read_textbox_input : unit -> string
     Returns the entered text. *)
 
 val open_textbox_with_prompt : string -> string
+(** [open_textbox_with_prompt prompt] opens a textbox with the given prompt.
+    [prompt] is the prompt to be displayed above the textbox. The function opens
+    a graphical window and displays a textbox with the specified prompt. It
+    allows the user to input text and handles key presses accordingly. It
+    returns the input text when the Enter key is pressed. *)
